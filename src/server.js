4f98a1bfb31d93faafcb1ff40f55c062c5a3dff7
lisa erksn <lisa.erksn@gmail.com>
2024-11-27 12:03:28 +0100

    update server

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT 
 
 //middleware
 app.use(express.json());
